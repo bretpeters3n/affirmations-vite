4fbc26a708802cf95f4ab1e9d58f4cbc6cc71d37
Bret Petersen <bretpetersen@gmail.com>
2024-01-16 14:41:22 -0600

    Continue working on correctly destructuring the defaultAffirmations array object

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
-import AffirmationParams from "./AffirmationParams";
+import AffirmationParams from "./components/AffirmationParams";
 
 const App = () => {
   return (
--- src/components/AffirmationParams.jsx
+++ src/components/AffirmationParams.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
-import Pet from "./Pet";
+import Pet from "../Pet";
 // import useBreedList from "./hooks/useBreedList";
 // import AFFIRMATIONS from "./db/affirmations";
-import defaultAffirmations from "./db/defaultAffirmations";
+import defaultAffirmations from "../db/defaultAffirmations";
 
 const AffirmationParams = () => {
   const [location, setLocation] = useState("");
   const [group, setGroup] = useState("Default Affirmations");
   //   const [breed, setBreed] = useState("");
   //   const [breeds] = useBreedList(affirmation);
-  const [affirmations, setAffirmations] = useState([]);
+  const [affirmationsList, setAffirmationList] = useState([]);
 
   useEffect(() => {
     requestAffirmations();
@@ -24,6 +24,16 @@ const AffirmationParams = () => {
   //     console.log("json.pets is: " + JSON.stringify(json.pets));
   //   }
 
+  const iterate = (obj) => {
+    Object.keys(obj).forEach((key) => {
+      console.log(`key: ${key}, value: ${obj[key]}`);
+
+      if (typeof obj[key] === "object" && obj[key] !== null) {
+        iterate(obj[key]);
+      }
+    });
+  };
+
   function requestAffirmations() {
     const res = defaultAffirmations;
     console.log(
@@ -32,6 +42,26 @@ const AffirmationParams = () => {
     );
     let currentGroup = "Default Affirmations";
     console.log("currentGroup is: " + JSON.stringify(currentGroup));
+
+    let affirmationList = defaultAffirmations[0].affirmations;
+    // let affirmationList = JSON.stringify(defaultAffirmations);
+    console.log("res is:");
+    console.log(JSON.stringify(res));
+    console.log("affirmationList is:");
+    console.log(JSON.stringify(affirmationList));
+    console.log("affirmationList[0] is:");
+    console.log(affirmationList[0]);
+    console.log("defaultAffirmations is:");
+    console.log(defaultAffirmations);
+    // console.log(iterate(affirmationList));
+    // setAffirmationList(affirmationList);
+
+    const index = affirmationList.findIndex((object) => {
+      // return object.id === "b";
+      return object.id === "Default Affirmations";
+    });
+
+    console.log(index);
   }
 
   return (
@@ -72,7 +102,7 @@ const AffirmationParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {/* {affirmations.map((group) => (
+      {/* {affirmationList.map((group) => (
         <Pet
           affirmation={group.affirmation}
           key={group.id}
