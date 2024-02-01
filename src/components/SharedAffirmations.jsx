f62588ff5e227ac41823127a25bb7b3c92d1a331
Bret Petersen <bretpetersen82@gmail.com>
2024-01-31 18:45:09 -0600

    Finish New User setup for Shared Affs page

--- src/components/SharedAffirmations.jsx
+++ src/components/SharedAffirmations.jsx
@@ -1,11 +1,22 @@
 import { useEffect, useState } from "react";
-// import { useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import SharedAffirmationResults from "./SharedAffirmationResults";
-import { requestAffirmationsDataIfPresent } from "../utils/PullPostGetSet";
+import {
+  requestAffirmationsDataIfPresent,
+  requestAndSaveAffirmationsData,
+} from "../utils/PullPostGetSet";
+import Group from "../utils/groupClass";
 import AffirmationParams from "./AffirmationParams";
+import ShortUniqueId from "short-unique-id";
+
+const uid = new ShortUniqueId();
 
 const SharedAffirmations = (props) => {
+  const navigate = useNavigate();
+
   const queryParameters = new URLSearchParams(window.location.search);
 
   let affirmationsData = null;
@@ -27,8 +38,47 @@ const SharedAffirmations = (props) => {
   const handleAcceptAffirmationsClick = () => {
     if (affirmationsData) {
       console.log("Add group to existing users affs");
+      /*
+      - Receiving affs as EXISTING USER
+      - Accept ⇒ Update localStorage save with added Received affs
+      - set currentGroup to Received affs group
+      - send user to DisplayAffs page with new affs playing
+      */
     } else {
       console.log("Create new user affs and add this new group");
+      affirmationsData = requestAndSaveAffirmationsData();
+      const id = affirmationsData[0].groups.length + 1;
+
+      // switch to
+      // const sharedGroup = sharedAffirmations;
+
+      affirmationsData[0].groups.push(JSON.parse(sharedAffirmations)[0]);
+      affirmationsData[0].currentGroup = currentGroup;
+      navigate("/");
+      toast.success(`Group '${currentGroup}' added!`, {
+        position: "bottom-center",
+      });
+      // affirmationsData[0].groups.push({
+      //   id: uid.rnd(),
+      //   group: newGroup.group,
+      //   affirmations: newGroup.affirmations,
+      // });
+
+      // let sharedAffirmationsNOTarray = JSON.parse(sharedAffirmations);
+      // console.log("sharedAffirmationsNOTarray is:");
+      // console.log(sharedAffirmationsNOTarray[0]);
+      // console.log(JSON.parse(sharedAffirmations)[0]);
+
+      // postAffirmationsData(affirmationsData);
+      // setShowModal(false);
+      // setCurrentGroup(newGroupName);
+
+      /*
+      - Receiving affs as NEW USER
+      - Accept ⇒ Create localStorage save with Received affs and Default Affs
+      - set currentGroup to Received affs group
+      - send user to DisplayAffs page with new affs playing
+      */
     }
   };
 
@@ -43,16 +93,6 @@ const SharedAffirmations = (props) => {
       - check for existing localStorage 'affirmationsUnique' key value pair
         - if (affirmationsUnique) { EXISTING USER }
         - else { NEW USER }
-
-    - Receiving affs as EXISTING USER
-      - Accept ⇒ Update localStorage save with added Received affs
-      - set currentGroup to Received affs group
-      - send user to DisplayAffs page with new affs playing
-
-    - Receiving affs as NEW USER
-      - Accept ⇒ Create localStorage save with Received affs and Default Affs
-      - set currentGroup to Received affs group
-      - send user to DisplayAffs page with new affs playing
 
     - Receiving affs as YOURSELF USER (likely a mistake, but code for)
       - Probs alert that you already have it? This brings up issues of same named groups and same ID groups. Regenerate ID upon Receiving affs?
