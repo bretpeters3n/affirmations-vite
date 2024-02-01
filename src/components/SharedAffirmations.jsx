8a0d5cd2011817d191df548c3b959a8c3ff3e2ff
Bret Petersen <bretpetersen82@gmail.com>
2024-02-01 15:53:37 -0600

    Refactor code that adds shared affirmations to affirmation storage

--- src/components/SharedAffirmations.jsx
+++ src/components/SharedAffirmations.jsx
@@ -38,68 +38,18 @@ const SharedAffirmations = (props) => {
   );
 
   const handleAcceptAffirmationsClick = () => {
-    if (affirmationsData) {
-      console.log("Add group to existing users affs");
-      const id = affirmationsData[0].groups.length;
-
-      affirmationsData[0].groups.push(JSON.parse(sharedAffirmations)[0]);
-      affirmationsData[0].currentGroup = currentGroup;
-      affirmationsData[0].groups[id].id = uid.rnd();
-
-      postAffirmationsData(affirmationsData);
-      navigate("/");
-      toast.success(`Group '${currentGroup}' added!`, {
-        position: "bottom-center",
-      });
-      /*
-      - Receiving affs as EXISTING USER
-      - Accept ⇒ Update localStorage save with added Received affs
-      - set currentGroup to Received affs group
-      - send user to DisplayAffs page with new affs playing
-      */
-    } else {
-      console.log("Create new user affs and add this new group");
+    if (!affirmationsData) {
       affirmationsData = requestAndSaveAffirmationsData();
-      const id = affirmationsData[0].groups.length;
-
-      affirmationsData[0].groups.push(JSON.parse(sharedAffirmations)[0]);
-      affirmationsData[0].currentGroup = currentGroup;
-      affirmationsData[0].groups[id].id = uid.rnd();
-
-      postAffirmationsData(affirmationsData);
-      navigate("/");
-      toast.success(`Group '${currentGroup}' added!`, {
-        position: "bottom-center",
-      });
-
-      // postAffirmationsData(affirmationsData);
-      // navigate("/");
-      // toast.success(`Group '${currentGroup}' added!`, {
-      //   position: "bottom-center",
-      // });
-
-      // affirmationsData[0].groups.push({
-      //   id: uid.rnd(),
-      //   group: newGroup.group,
-      //   affirmations: newGroup.affirmations,
-      // });
-
-      // let sharedAffirmationsNOTarray = JSON.parse(sharedAffirmations);
-      // console.log("sharedAffirmationsNOTarray is:");
-      // console.log(sharedAffirmationsNOTarray[0]);
-      // console.log(JSON.parse(sharedAffirmations)[0]);
-
-      // postAffirmationsData(affirmationsData);
-      // setShowModal(false);
-      // setCurrentGroup(newGroupName);
-
-      /*
-      - Receiving affs as NEW USER
-      - Accept ⇒ Create localStorage save with Received affs and Default Affs
-      - set currentGroup to Received affs group
-      - send user to DisplayAffs page with new affs playing
-      */
     }
+    const id = affirmationsData[0].groups.length;
+    affirmationsData[0].groups.push(JSON.parse(sharedAffirmations)[0]);
+    affirmationsData[0].currentGroup = currentGroup;
+    affirmationsData[0].groups[id].id = uid.rnd();
+    postAffirmationsData(affirmationsData);
+    navigate("/");
+    toast.success(`Group '${currentGroup}' added!`, {
+      position: "bottom-center",
+    });
   };
 
   /*
