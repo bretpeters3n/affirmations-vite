a5d4679ab2dfd46c324d8ae08d0625dddf0873ec
Bret Petersen <bretpetersen@gmail.com>
2024-02-03 23:53:27 -0600

    Post first gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/affirmations-vite",
   plugins: [react()],
   root: "src",
 });
